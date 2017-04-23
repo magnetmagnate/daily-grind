f1957debed0c51a5cdce37cd8e82b7aa1be31c87
Marcus Meng <aenslad.mackenzie@gmail.com>
2017-04-22 17:03:42 -0700

    Write to plain text file test.

--- driver.py
+++ driver.py
@@ -15,5 +15,10 @@ def repeat_print(to_print, interval):
 entry = input("Enter some stuff: ")
 delay = float(input("Input repeat interval in seconds: "))
 
+# Dump stuff in a file for testing.
+with open('data.txt', 'w') as data_file:
+    print(entry, file=data_file)
+
+# Repeat forever at interval.
 repeat_print(entry, delay)
 
