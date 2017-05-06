6982071f02f75c8694385faaae8b72c09e72b25d
M.J. Gardner <magnetmagnate@users.noreply.github.com>
2017-05-05 22:44:03 -0700

    add an object definition for task entries to write to json

--- src/driver.py
+++ src/driver.py
@@ -9,20 +9,35 @@ s = sched.scheduler(time.time, time.sleep)
 # Scheduler test nonsense
 
 
+class TaskEntry(object):
+    """
+    An entry for a task to be tracked
+    """
+
+    def __init__(self, title, resetduration):
+        self.title = title
+        self.resetduration = resetduration
+
+
+def jsonDefault(object):
+    return object.__dict__
+
+
 def repeat_print(to_print, interval):
     print(to_print)
     s.enter(interval, 1, repeat_print, argument=(to_print, interval))
     s.run()
 
+
 title = input("Enter some stuff: ")
 delay = float(input("Input repeat interval in seconds: "))
 
-entries = [("test1", 5.0)]
-entries.append((title, delay))
+entries = [TaskEntry("test1", 5.0)]
+entries.append(TaskEntry(title, delay))
 
 # Dump stuff in a file for testing.
 with open('data.json', 'w') as data_file:
-    json.dump(entries, data_file)
+    json.dump(entries, data_file, default=jsonDefault)
 
 # Repeat forever at interval.
 repeat_print(title, delay)
