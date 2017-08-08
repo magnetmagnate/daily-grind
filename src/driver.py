674c8ced128070e019b666b25adc2700441bef1a
M.J. Gardner <magnetmagnate@users.noreply.github.com>
2017-08-07 22:46:16 -0700

    add reset time tracking to driver.py. almost got something sorta functional now

--- src/driver.py
+++ src/driver.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
+# built-in modules
+from datetime import datetime, timedelta
+from pprint import pprint
 import sched
 import time
-import json
-from pprint import pprint
+import argparse
+
+# modules from packages
+from pytz import timezone, utc
+import pytz
+from json_tricks.nonp import dump, load
 
 s = sched.scheduler(time.time, time.sleep)
 
@@ -13,40 +20,48 @@ class TaskEntry(object):
     An entry for a task to be tracked
     """
 
-    def __init__(self, title, resetduration):
+    def __init__(self, title, resetduration, lastreset=(datetime.min)):
         self.title = title
         self.resetduration = resetduration
+        self.lastreset = lastreset
 
 
-def jsonDefault(object):
-    """
-    Translate an object into a dict in order to write to json
-    """
-    return object.__dict__
+parser = argparse.ArgumentParser(
+    description='Time since last checked and stuff.')
+# if we get this argument, mark all tasks as completed
+parser.add_argument('--reset_all', action='store_true')
 
-
-def repeat_print(to_print, interval):
-    print(to_print)
-    s.enter(interval, 1, repeat_print, argument=(to_print, interval))
-    s.run()
-
+args = parser.parse_args()
 
 # Read the stuff from a file.
 with open('tasks.json') as tasks_file:
-    tasks = json.load(tasks_file)
+    tasks = load(tasks_file)
     pprint(tasks)
 
-# Print all dat stuff in the file
+cur_time = datetime.now()
+print("\nCurrent time: " + str(cur_time))
+# Print some info about all dat stuff in the file and update reset time
 for task in tasks:
-    print(task["title"] + str(task["resetduration"]))
+    if 'lastreset' not in task:  # in case we have a new task
+        task['lastreset'] = datetime.min
+    print("\n\n" + task['title'] + " " +
+          str(task['resetduration']) + " " + str(task['lastreset']))
+    print("\nTime since last completed:")
+    print(cur_time - task['lastreset'])
+    if (cur_time - task['lastreset']).days >= 1:
+        print("Longer than 1 day since last run.")
+    else:
+        print("Not longer than 1 day since last run")
+    if (cur_time - task['lastreset']) >= task['resetduration']:
+        print("Longer than " + str(task['resetduration']) + " since last run.")
+    else:
+        print("Not longer than " +
+              str(task['resetduration']) + " since last run.")
+    if args.reset_all:
+        print("Resetting all tasks.")
+        task['lastreset'] = cur_time
 
-title = tasks[0]["title"]
-delay = float(tasks[0]["resetduration"])
 
-# Dump stuff in a file for testing.
+# Dump stuff back in the file
 with open('tasks.json', 'w') as tasks_file:
-    json.dump(tasks, tasks_file, default=jsonDefault, indent=2)
-
-# Repeat forever at interval.
-repeat_print(title, delay)
-
+    dump(tasks, tasks_file)
